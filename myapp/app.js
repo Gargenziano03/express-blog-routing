2c76689cd27b522aafa7509ff1b646cf9e4b5c5a
Gargenziano03 <gargenziano03@gmail.com>
2024-11-04 16:31:36 +0100

    add import/export posts + endpoints

--- myapp/app.js
+++ myapp/app.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const app = express()
+const posts = require('./db/db.js')
 
 // static files
 app.use(express.static('/public'))
 
 app.listen(3000, () => {
     console.log("Server started on port 3000")
+});
+
+// Let's now create our first endpoints
+// (index) Get all posts
+
+app.get('/posts', (req, res) => {
+    res.json({
+        data: posts,
+        count: posts.length
+    })
 });
