174cd3f25bd0f284d48228c8e7266a71348e0f3b
Gargenziano03 <gargenziano03@gmail.com>
2024-11-05 08:56:52 +0100

    add find the post by id

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,7 +6,15 @@ const index = (req, res) => {
         count: posts.length
     })
 }
+const show = (req, res) => {
+    const post = posts.find((post) => post.id === parseInt(req.params.id))
+    if (!post) {
+      return res.status(404).json({ error: "No pizza found with that id" })
+    }
+    return res.status(200).json({ data: post })
+  }
 
 module.exports = {
-    index
+    index,
+    show
 }
